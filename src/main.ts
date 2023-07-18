6ab7faf6739d5639ba1ae8abdbcb535981446b74
Marcus Roza <marcusroza@pop-os.localdomain>
2023-07-18 02:16:42 -0300

    deploy up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cors from 'cors'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
